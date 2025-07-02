3822255c77f45ece1c3668776ae28b8d5e977f52
Wittmann-MEE <mee.wiku@wittmann-group.com>
2025-07-02 12:29:23 +0200

    utils/parsing_utils: Add few more cases and improve robustness

--- src/kiutils/utils/parsing_utils.py
+++ src/kiutils/utils/parsing_utils.py
@@ -3,21 +3,23 @@ from typing import Union
 def parse_bool(item: Union[list, str], key: str) -> bool:
     """
     Parses a boolean from either old or new format:
-    - Old: item is a string like 'hide'
+    - Old: item is a string like 'hide' or ['hide']
     - New: item is a list like ['hide', 'yes'/'no']
     Returns True if the key matches and the value is "true".
     """
     if isinstance(item, str):
         return item == key
+    elif isinstance(item, list) and len(item) == 1:
+        return item[0] == key
     elif isinstance(item, list) and len(item) == 2:
         return item[0] == key and item[1].lower() == "yes"
 
 
-def format_bool(key: str, value: bool, compact: bool = False) -> str:
+def format_bool(key: str, value: bool, compact: bool = False, yesno: bool = False) -> str:
     if not isinstance(value, bool):
         raise TypeError(f"Expected a boolean value, got {type(value).__name__}")
-    if not value:
+    if not yesno and not value:
         return ""
     else:
-        return f"({key})" if compact else f"({key} yes)"
+        return key if compact else (f"({key} yes)" if not yesno else f"({key} {'yes' if value else 'no'})")
 
